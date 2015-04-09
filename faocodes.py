eb7e9a41aaed15d7c85abb8636eefa07f4b3197e
me <me@no.com>
2015-04-10 02:20:29 +0800

    added reverse lookup of codes

--- faocodes.py
+++ faocodes.py
@@ -13,7 +13,7 @@ import os, json, urllib
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def FaostatGetDictionary():
+def GetDictionary():
     # loads the main dictionary from members.json
     prop_dict = {}
     temp_list = []
@@ -24,7 +24,15 @@ def FaostatGetDictionary():
         temp_list.append(item["properties"])
     return prop_dict
 
-def FaostatCodeQuery(search_string, prop_dict):
+def QueryForProperties(code, prop_dict):
+    # gets the properties for any member(s) matching the numerical code
+    results_list = []
+    for key in prop_dict:
+        if int(prop_dict[key]["bk"]) == int(search_string):
+            results_list.append(prop_dict[key])
+    return results_list
+
+def QueryForCodes(search_string, prop_dict):
     # gets labels and codes from the dictionary based on a simple string search
     pair_list = [] 
     for key in prop_dict:
@@ -35,13 +43,34 @@ def FaostatCodeQuery(search_string, prop_dict):
             pair_list.append(result)
     return pair_list
 
+def OutputProperties(code, results_list):
+    if not results_list:
+        print
+        print "No matches found for code", code
+    else:
+        for result in results_list:
+            # clean up the label
+            label = result["label"]
+            label = label.replace("&apos;", "'")
+            label = label.replace("&amp;", "&")
+            if "description" in result:
+                # clean up the description
+                descrip = result["description"]
+                descrip = descrip.replace("&apos;", "'")
+                descrip = descrip.replace("&amp;", "&")
+                # and now the output
+                print
+                print "Item", code+":" 
+                print label
+                print
+                print "Description: ", descrip
+            else:
+                print
+                print "Region", code+":"
+                print label
 
-if __name__ == '__main__':
-    prop_dict = FaostatGetDictionary()
-    search_string = raw_input("Enter your search string: ")
-    code_list = FaostatCodeQuery(search_string, prop_dict)
-    code_list = sorted(code_list)
-    print ""
+def OutputCodeList(code_list):
+    print "\n"
     for result in code_list:
         label = result[0]
         label = label.replace("&amp;", "&")
@@ -58,3 +87,13 @@ if __name__ == '__main__':
         else:
             output_line = label+"\t"+code
         print output_line
+
+if __name__ == '__main__':
+    search_string = raw_input("Enter your search string or numerical code: ")
+    prop_dict = GetDictionary()
+    if search_string.isdigit():
+        properties_list = QueryForProperties(search_string, prop_dict)
+        OutputProperties(search_string, properties_list)
+    else:
+        code_list = sorted(QueryForCodes(search_string, prop_dict))
+        OutputCodeList(code_list)
