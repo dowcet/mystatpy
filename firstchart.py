e1f1a418f27d64225217e5bb977609e8f3cc6c19
me <me@no.com>
2015-04-10 15:19:08 +0800

    tested reading, narrowing, and writing of data

--- firstchart.py
+++ firstchart.py
@@ -1,15 +1,26 @@
 import pandas, os
 
-def FirstRead():
-    # reading this huge csv file is slow, this will get the data for the regions I need and save it as a hdf5 file
+def MakeH5():
+    # reading this huge csv file is slow, but this will get the data for the regions I need and save it as an hdf5 file
     #
-    # read the csv
-    raw_df = pandas.DataFrame.from_csv("~/dissertation/stats/downloads/faostat/E_production_crops_all.csv")
+    # 1) read the csv
+    raw_df = pandas.DataFrame.from_csv("E_production_crops_all.csv")
     #
-    # filter by country
-    filtered = raw_df.loc[raw_df["Country"].isin(["Malaysia", "Indonesia", "World"])]
+    # 2) filter by country
+    narrowed = raw_df.loc[raw_df["Country"].isin(["Malaysia", "Indonesia", "World"])]
     #
-    # save to hdf5
-    output = os.getcwd()+"mydata.h5"
-    raw_df.to_hdf(output, 'data', mode='w', format='fixed') 
+    # 3) save to hdf5 and clear memory
+    output = "narrowed.h5"
+    narrowed.to_hdf(output, 'data', mode='w', format='fixed') 
     del raw_df
+
+# This one is not ready yet
+def GetSeries():
+    # Load the file 
+    df = pandas.read_hdf("narrowed.h5", 'data')
+    # TODO now 
+    
+# Test confirms that MakeH5 worked by reading the data
+df = pandas.read_hdf("narrowed.h5", 'data')
+print df.head   
+
