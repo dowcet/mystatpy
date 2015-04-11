380ce0234e314d4bf0e968e7ae9d79b55b7ee3cd
me <me@no.com>
2015-04-11 14:12:31 +0800

    adding comments, checking out the data

--- firstchart.py
+++ firstchart.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+#
+# MakeH5: reads a CSV, makes a smaller dataframe based on a query using .loc, and then writes an H5 file
+# GetSeries: loads the resulting file H5 file and gets the series to be plotted
+# PlotSeries: displays a plot of the series
+#
+# TODO see below
+
 import pandas, os
 
 def MakeH5():
 # reading this huge csv file is slow, but this will get the data for the regions I need and save it as an hdf5 file
-    
+
     # read the csv
     csv_file_name = "E_production_crops_all.csv"
     print "Reading from file", csv_file_name, "..."
@@ -12,7 +20,7 @@ def MakeH5():
     raw_df = raw_df.reset_index()
 
     #TODO write a dictionary to a json file based on "Country", "Item", "Element"
-    #TODO drop those columns (and also "Year Code") from the df
+    #TODO delete those columns (and also "Year Code") from the df
     
     # check
     print raw_df.head(3)
@@ -26,7 +34,7 @@ def MakeH5():
     print narrow_df.describe()
 
     # save to hdf5 and clear memory
-    output = "production_crops_narrowed.h5"
+    output = "E_select_crops.h5"
     print "Saving to", output, "..."
     narrow_df.to_hdf(output, 'data', mode='w', format='fixed') 
     del narrow_df
@@ -34,15 +42,25 @@ def MakeH5():
 # This one is not ready yet
 def GetSeries():
     # Load the file 
-    raw_df = pandas.read_hdf("production_crops_narrowed.h5",'data')
+    print "Reading h5 file..." 
+    raw_df = pandas.read_hdf("E_select_crops.h5",'data')
+    item_code_list = [254, 256, 257] # tried 258 and 259 but they were empty
+    for item_code in item_code_list:
+        temp_df = raw_df.loc[raw_df["Item Code"].isin([item_code])]
+        print "Showing data for Item Code", item_code, "..."
+        print
+        print temp_df.describe()
     # TODO narrow again by item
     # TODO create make the series
     # TODO plot the series
+
+# The following can be run as main routines 
 
 # narrow and convert the data, only need to do this once
 # MakeH5()
         
 # Test confirms that MakeH5 worked by reading the data
-print "Reading h5 file..." 
-df = pandas.read_hdf("production_crops_narrowed.h5", 'data')
-print df.describe()
+# df = pandas.read_hdf("E_select_crops.h5", 'data')
+# print df.describe()
+
+GetSeries()
